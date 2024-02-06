a1da2ee952a9e3f9e38b6a93dced3afed1d53ba4
Steve Curtis <steve.curtis@truthmd.com>
2024-02-06 14:20:59 -0500

    Add material module and add routing

--- foo/src/app/app.module.ts
+++ foo/src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MaterialModule } from "./common/material.module";
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  // { path: 'dashboard', component: DashboardComponent },
+  // { path: 'detail', component: DetailComponent },
+  { path: '**', redirectTo: 'dashboard' }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
