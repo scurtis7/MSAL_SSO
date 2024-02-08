67828b4f403b6adcf9fb05dd40c48c00c7a030f4
Steve Curtis <steve.curtis@truthmd.com>
2024-02-08 16:06:01 -0500

    Add and configure initial msal code

--- bar/src/app/app.module.ts
+++ bar/src/app/app.module.ts
@@ -3,12 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { RouterModule, Routes } from "@angular/router";
 import { MaterialModule } from "./common/material.module";
+import { RouterModule, Routes } from "@angular/router";
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MenuComponent } from './components/shared/menu/menu.component';
 import { DetailComponent } from './components/detail/detail.component';
 import { LoginComponent } from './components/login/login.component';
+import { BrowserUtils, InteractionType, IPublicClientApplication, PublicClientApplication } from "@azure/msal-browser";
+import { MSAL_GUARD_CONFIG, MSAL_INSTANCE, MsalBroadcastService, MsalGuard, MsalGuardConfiguration, MsalModule, MsalRedirectComponent, MsalService } from "@azure/msal-angular";
+import { msalConfig } from "./auth/auth-config";
+
+/**
+ * Documentation for MSAL
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-angular/docs/configuration.md
+ */
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication(msalConfig);
+}
+
+/**
+ * Set your default interaction type for MSALGuard here. If you have any
+ * additional scopes you want the user to consent upon login, add them here as well.
+ */
+export function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return {
+    interactionType: InteractionType.Redirect
+  };
+}
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
@@ -28,11 +49,30 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     MaterialModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes,
+      {
+        // Don't perform initial navigation in iframes or popups
+        initialNavigation:
+          !BrowserUtils.isInIframe() && !BrowserUtils.isInPopup()
+            ? "enabledNonBlocking" : "disabled", // Set to enabledBlocking to use Angular Universal
+      }),
+    MsalModule
   ],
   providers: [
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: MSALGuardConfigFactory,
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, MsalRedirectComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ bar/src/app/auth/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { MsalService } from "@azure/msal-angular";
+import { CacheService } from "../service/cache.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private msalService: MsalService, private cacheService: CacheService) {
+  }
+
+  login(): void {
+    this.msalService.loginRedirect();
+  }
+
+  logout(): void {
+    this.msalService.logoutRedirect();
+    this.cacheService.clearAllCache();
+  }
+
+  isUserLoggedIn(): boolean {
+    return this.isTokenValid();
+  }
+
+  private isTokenValid(): boolean {
+    const currentTime = Math.floor(Date.now() / 1000);  // current epoch time in seconds
+    const expireTime = this.cacheService.getTokenExpiresOn();
+    // console.log("Current Time->" + currentTime + "   Expire Time->" + expireTime);
+    if (expireTime === undefined) {
+      return false;
+    }
+    return expireTime > currentTime;
+  }
+
+}
